88873bc515a49664c004de5e64a707d97afd788d
dsicacha <sica0902@gmail.com>
2020-10-21 00:01:54 -0500

    .

--- src/models/transmision.js
+++ src/models/transmision.js
@@ -4,7 +4,7 @@ const {Schema, model} = require('mongoose');
 const transmisionSchema = new Schema({
     usuario:{type:String,required:true},
     origen:{type: Schema.Types.ObjectId, ref: 'origen'},
-    origen:{type: Schema.Types.ObjectId, ref: 'destino'}
+    destino:{type: Schema.Types.ObjectId, ref: 'destino'}
 },{
     timestamps:true,
     versionKey:false
