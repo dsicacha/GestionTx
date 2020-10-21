94abca8eac1ddde51d9d070ad6fcd772d7655782
dsicacha <sica0902@gmail.com>
2020-10-20 22:59:35 -0500

    ajustes a los modelos

--- src/models/destino.js
+++ src/models/destino.js
@@ -4,21 +4,26 @@ const destinoSchema = new Schema({
     nombre:{type:String,required:true},
     telefono:{type:String,required:true},
     email:{type:String,required:true},
+    usuarioRed:{type:String,required:true},
 
-    filial:{type:Number,required:true},
-    dominio:{type:Number,required:true},
-    subdominio:{type:Number,required:true},
-    proceso:{type:Number,required:true},
-    codigoMac:{type:Number,required:true},
+    filial:{type:String,required:true},
+    dominio:{type:String,required:true},
+    subdominio:{type:String,required:true},
+    proceso:{type:String,required:true},
+    codigoMac:{type:String,required:true},
 
-    ambiente:{type:Number,required:true},
-    protocoloTransmision:{type:Number,required:true},
-    puerto:{type:Number,required:true},
-    Servidor:{type:Number,required:true},
-    usuarioConexionDestino:{type:Number,required:true},
-    metodoAutenticacion:{type:Number,required:true},
-    ubicacionServidor:{type:Number,required:true},
-    rutaEntrega:{type:Number,required:true}
+    ambiente:{type:String,required:true},
+    usuarioCnx:{type:String,required:true},
+
+
+    protocoloTransmision:{type:String,required:true},
+    puerto:{type:String,required:true},
+    tipoSvr:{type:String,required:true},
+    Servidor:{type:String,required:true},
+    usuarioConexionDestino:{type:String,required:true},
+    metodoAutenticacion:{type:String,required:true},
+    ubicacionServidor:{type:String,required:true},
+    rutaEntrega:{type:String,required:true}
     
 },{
     timestamps:true,
